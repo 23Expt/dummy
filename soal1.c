cd8fad18fd324e813438e901236f22efd8644214
Al Fachri Sagianto <163378556+23Expt@users.noreply.github.com>
2024-06-03 22:44:22 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+// Function executed by each thread
+void *check_prime(void *arg) {
+    int num = *((int *)arg);
+    if (is_prime(num)) {
+        printf("%d prima\n", num);
+    } else {
+        printf("%d bukan prima\n", num);
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threads[argc - 1];
+    for (int i = 1; i < argc; i++) {
+        int num = atoi(argv[i]);
+        pthread_create(&threads[i - 1], NULL, check_prime, (void *)&num);
+    }
+    for (int i = 0; i < argc - 1; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    return 0;
+}
